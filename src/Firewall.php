f0644e3bd08d63833a10112bee06393bb338ecc8
Sean Dietrich <sean@kanopi.com>
2025-06-18 15:18:51 -0700

    Updated docblocks on all files

--- src/Firewall.php
+++ src/Firewall.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Firewall package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Kanopi\Firewall;
 
 use Kanopi\Firewall\Logging\LoggingFactory;
--- src/Plugins/DatabaseTrait.php
+++ src/Plugins/DatabaseTrait.php
@@ -1,5 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Firewall package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Kanopi\Firewall\Plugins;
 
 use Doctrine\DBAL\Connection;
