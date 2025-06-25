7a7541f9e1f4810a3361b983be907f0506b8bddd
Sean Dietrich <sean@kanopi.com>
2025-06-25 15:01:21 -0700

    Update geoip script

--- bin/update_geoip.sh
+++ bin/update_geoip.sh
@@ -17,6 +17,15 @@ fi
 
 cd $DIR
 
+# Using the following mirror to download files.
+curl -fsSL -O https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-ASN.mmdb
+curl -fsSL -O https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb
+curl -fsSL -O https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-Country.mmdb
+
+exit 0
+
+
+# Used as a way to download directory from MaxMind site.
 CURRENT=$(pwd)
 
 for EDITION in "GeoLite2-City" "GeoLite2-ASN" "GeoLite2-Country"
