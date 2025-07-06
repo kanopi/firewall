af59bee34a43e4e7a6cb22e9934370da35fb2a40
Sean Dietrich <sean@kanopi.com>
2025-07-05 22:54:41 -0700

    Updated the cookies as that is case sensitive

--- src/Firewall.php
+++ src/Firewall.php
@@ -308,9 +308,9 @@ final readonly class Firewall
 
                 // 5. request.cookie.<name>
                 if (str_starts_with($key, 'request.cookie.')) {
-                    $key = $m[1];
                     $param = substr($key, 15);
-                    return (string) $request->cookies->get($param, '');
+                    $cookies = array_change_key_case($request->cookies->all());
+                    return strval($cookies[$param] ?? '');
                 }
 
                 // 6. Arbitrary context values ----------------------------------
