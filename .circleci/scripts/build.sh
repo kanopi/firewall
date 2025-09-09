a33c73b0caad50eb3f93aea2d33eb780c5b33e4d
Sean Dietrich <sean@kanopi.com>
2025-09-09 10:03:17 -0700

    Fixed build script

--- .circleci/scripts/build.sh
+++ .circleci/scripts/build.sh
@@ -6,7 +6,7 @@ set -e
 APP_ROOT=$(pwd)
 BUILD_DIR="/tmp/firewall"
 PHP_VERSION_DEFAULT=$(php -r '$version=explode(".", phpversion()); echo sprintf("%s.%s", $version[0], $version[1]);' | tr -d '[:space:]' )
-PHP_VERSION=$(PHP_VERSION:-$PHP_VERSION_DEFAULT)
+PHP_VERSION=${PHP_VERSION:-$PHP_VERSION_DEFAULT}
 ZIP_FILE="/tmp/firewall-${PHP_VERSION}.zip"
 
 # Step 1: Clean up previous builds
